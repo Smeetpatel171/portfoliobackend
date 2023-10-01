a88eeb066689223e39f25edd26311b036006fbbc
Smeet Patel <smeetpatel171@outlook.com>
2023-10-02 02:06:57 +0530

    error handling modified

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ router.post("/register", (req, res) => {
       },
     });
 
-    const mailOptions = {
+    const mailOptions = { 
       to: "sppatelmaster123@gmail.com",
       subject: "You have a Cient SP",
       html: `<h1>Client/Recruiter Details</h1><h2>Name : ${name}</h2><h2>email : ${email}</h2><h2>Subject : ${subject}</h2><h2>Message : ${message}</h2>`,
@@ -32,7 +32,8 @@ router.post("/register", (req, res) => {
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         console.log("Error:", error);
-        res.send("error occured");
+         // Instead of sending plain text, send a JSON response with an error message.
+         res.status(500).json({ error: "An error occurred while sending the email" });
       } else {
         console.log("Email Sent" + info.response);
         res.status(201).json({ status: 201, info });
